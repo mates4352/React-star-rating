fa0077c718b9a0e452a1396e4df294ee4d89d600
SergeySabyrkin <sergeysabyrkin@gmail.com>
2022-02-07 22:51:29 +0300

    Add props onClick in component Star

--- src/components/reacting/Reacting.tsx
+++ src/components/reacting/Reacting.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Star } from "../star/Star";
 import './Reacting.css'
 
-export type valueActiveStar = number | 0 | 1 | 2 | 3 | 4 | 5;
+export type valueActiveStar =  0 | 1 | 2 | 3 | 4 | 5;
 
 
 export type reactingType = {
@@ -13,11 +13,11 @@ export type reactingType = {
 export const Reacting:React.FC<reactingType> = ({value, changeValueStar}) => {
   return (
     <div className="reacting">  
-        <Star selected={value > 0}/> <button className="button" onClick={ ()=> changeValueStar(1)}>1</button>
-        <Star selected={value > 1}/> <button className="button" onClick={ ()=> changeValueStar(2)}>2</button>
-        <Star selected={value > 2}/> <button className="button" onClick={ ()=> changeValueStar(3)}>3</button>
-        <Star selected={value > 3}/> <button className="button" onClick={ ()=> changeValueStar(4)}>4</button>
-        <Star selected={value > 4}/> <button className="button" onClick={ ()=> changeValueStar(5)}>5</button>
+        <Star selected={value > 0} onClick={()=> changeValueStar(1)}/>
+        <Star selected={value > 1} onClick={()=> changeValueStar(2)}/>
+        <Star selected={value > 2} onClick={()=> changeValueStar(3)}/>
+        <Star selected={value > 3} onClick={()=> changeValueStar(4)}/>
+        <Star selected={value > 4} onClick={()=> changeValueStar(5)}/>
     </div>
   );
 }
--- src/components/star/Star.tsx
+++ src/components/star/Star.tsx
@@ -3,13 +3,13 @@ import './Star.css'
 
 export type starType = {
   selected: Boolean
+  onClick: () => void
 }
 
-
-export const Star:React.FC<starType> = ({selected}) => {
+export const Star:React.FC<starType> = ({selected, onClick}) => {
   const isClassActive = selected ? "star star--active" : "star";
   return (
-    <div className={isClassActive}>
+    <div className={isClassActive} onClick={onClick}>
         Star
     </div>
   );
